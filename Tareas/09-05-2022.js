cecd1e32f6fd012ef9bc180f78263fb374292e2b
Juan <sebas981@live.com>
2022-05-16 12:32:44 -0300

    creacion de reloj

---
+++ Tareas/09-05-2022.js
@@ -0,0 +1,21 @@
+const mostrarFecha = function(){
+    setTimeout(function() {
+       fecha = new Date();
+       console.log(fecha);
+       document.write(fecha);
+    }, 3000);
+ }
+ mostrarFecha();
+ 
+ function iniciarRecorrido(){
+    calorias=0
+    contador=0;
+    cronometro = setInterval(function(){
+       calorias=calorias+0.15;
+       contador++;
+       console.log(`Estás quemando: ${Math.round(calorias)} calorias en ${contador} segundos`)
+    },1000);
+ }
+ const detenerRecorrido = function(){
+    clearInterval(cronometro);
+ }
--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const mostrarFecha = function(){
-   setTimeout(function() {
-      fecha = new Date();
-      console.log(fecha);
-      document.write(fecha);
-   }, 3000);
-}
-mostrarFecha();
+let numAle = 0;
+const playGame = function (){
+    numAle = Math.ceil(Math.random()*10);
+    console.log(numAle);
+    document.getElementById('play').disabled=true;
+};
+function probarNumero(){
+    let numJugador = document.getElementById('numJugador').value; //con value capturamos el valor del imput
+    console.log(numJugador);
+    if(numJugador==numAle){
+        alert('Ganaste!!! 😎')
+        document.getElementById('numJugador').value='';
+        document.getElementById('play').disabled=false;
+    }else if(numJugador > numAle){
+        alert('El número ingresado es MAYOR que el número mágico')
+        document.getElementById('numJugador').select();
 
-function iniciarRecorrido(){
-   calorias=0
-   contador=0;
-   cronometro = setInterval(function(){
-      calorias=calorias+0.15;
-      contador++;
-      console.log(`Estás quemando: ${Math.round(calorias)} calorias en ${contador} segundos`)
-   },1000);
-}
-const detenerRecorrido = function(){
-   clearInterval(cronometro);
-}
+    }else{
+        alert('El número ingresado es MENOR que el número mágico')
+    }
+} 
